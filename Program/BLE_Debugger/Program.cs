07cbaffa7760d535dc3a63158df3632df28811d4
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-05-04 20:02:46 +0900

    BLE通信を用いてデバッグを行うためのWindowsアプリです

---
+++ Program/BLE_Debugger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using Windows.Devices.Bluetooth;
+
+namespace BLE_Debugger
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
