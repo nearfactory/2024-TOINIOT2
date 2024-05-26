1f56809608ab9bf2dd7f4584172bc2fc3fa84a4b
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-05-26 21:47:38 +0900

    Wiki用サンプルプログラムを更新した

--- Program/I2C_Sample/ESP32C3/ESP32C3.ino
+++ Program/I2C_Sample/ESP32C3/ESP32C3.ino
@@ -1,5 +1,8 @@
+//ESP32C3(スレーブ)側コード
+
 #include<Wire.h>
 
+using uint8_t = unsigned char;
 constexpr uint8_t ESP32C3_ADDR = 0x32;
 
 int send_ms=0;
@@ -12,43 +15,44 @@ void setup() {
   Serial.begin(9600);
   Serial.println("ESP32C3");
 
-  Wire.begin(ESP32C3_ADDR);
-  Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
+  Wire.begin(ESP32C3_ADDR);                             // ESP32C3_ADDR(0x32)としてI2Cを開始
+  Wire.onReceive(receiveEvent);                         // マスターからの受信時のコールバック関数を設定
+  Wire.onRequest(requestEvent);                         // マスターからの送信リクエスト時のコールバック関数を設定
 }
 
 void loop() {
-  Serial.printf("Send : \"%s\"(%dms), Receive : \"%s\"(%dms) \n", send_str.c_str(), send_ms, receive_str.c_str(), receive_ms);
+  // 送受信結果を出力
+  Serial.printf("Send : \"%s\"(%dms),  Receive : \"%s\"(%dms) \n", send_str.c_str(), send_ms, receive_str.c_str(), receive_ms);
   delay(100);
 }
 
 // マスターからの送信に対するコールバック関数
 void receiveEvent(int size){
-  int previous_receive_ms = millis();
+  int previous_receive_ms = millis();                   // I2Cの受信開始時刻を取得
 
-  receive_str = "";
-  while(Wire.available()>0){
-    char c = Wire.read();
-    receive_str += c;
+  receive_str = "";                                     // 受信文字列をクリア
+  while(Wire.available()){                              // 受信バッファが空になるまで繰り返す
+    char c = Wire.read();                               // 受信バッファから1文字取り出す
+    receive_str += c;                                   // 受信文字列の末尾に追加
   }
 
-  receive_ms = millis() - previous_receive_ms;
+  receive_ms = millis() - previous_receive_ms;          // 開始時刻との差分を取って受信にかかった時間を計測
 
   return;
 }
 
 // マスターからのリクエストに対するコールバック関数
 void requestEvent(){
-  int previous_send_ms = millis();
-  static int count=0;
+  int previous_send_ms = millis();                      // リクエストの開始時刻を取得
+  static int count=0;                                   // カウンタを宣言
   
-  send_str = "ESP32C3 : " + std::to_string(count/2) + "\0";
-  auto length = static_cast<char>(send_str.length());
-  send_str =  length + send_str;
-  count++;
-  Wire.write(send_str.c_str());
+  send_str = "ESP32C3 : " + std::to_string(count/2);    // 送信文字列を準備
+  auto length = static_cast<char>(send_str.length());   // 文字列の長さをchar型(文字)として取得
+  send_str =  length + send_str;                        // 送信文字列の先頭に、上の文字数を示すデータを追加
+  count++;                                              // カウンタを1増やす
+  Wire.write(send_str.c_str());                         // 送信バッファに追加
 
-  send_ms = millis() - previous_send_ms;
+  send_ms = millis() - previous_send_ms;                // 開始時刻との差分を取ってリクエストの処理にかかった時間を計測
 
   return;
 }
--- Program/I2C_Sample/Teensy4.1/Teensy4.1.ino
+++ Program/I2C_Sample/Teensy4.1/Teensy4.1.ino
@@ -1,7 +1,10 @@
+//Teensy4.1(マスター)側コード
+
 #include<string>
 
 #include<Wire.h>
 
+using uint8_t = unsigned char;
 constexpr uint8_t ESP32C3_ADDR = 0x32;
 
 void setup() {
@@ -14,26 +17,25 @@ void setup() {
 void loop() {
   static int count=0;
   count++;
-  std::string send_str = "Teensy4.1 : " + std::to_string(count);
+  std::string send_str = "Teensy4.1 : " + std::to_string(count);  //送信データを準備
 
   // スレーブへデータを送信
-  int previous_send_ms = millis();
-  Wire.beginTransmission(ESP32C3_ADDR);
-  Wire.write(send_str.c_str());
-  Wire.endTransmission();
-  int send_ms = millis() - previous_send_ms;
+  int previous_send_ms = millis();                  // I2Cの送信開始時刻を取得
+  Wire.beginTransmission(ESP32C3_ADDR);             // ESP32C3との通信準備を開始
+  Wire.write(send_str.c_str());                     // データを送信バッファに追加
+  Wire.endTransmission();                           // 送信
+  int send_ms = millis() - previous_send_ms;        // 開始時刻との差分を取って通信時間を計測
 
   // スレーブへデータをリクエスト
-  int previous_receive_ms = millis();
-  std::string receive_str="";
-  Wire.requestFrom(ESP32C3_ADDR,128);
-  unsigned char received_size = Wire.read();
-  for(int i=0;i<received_size;i++){
-    char c = Wire.read();
-    receive_str += c;
-    if(c=="\0") break;
+  int previous_receive_ms = millis();               // I2Cのリクエスト開始時刻を取得
+  std::string receive_str="";                       // 受信データ格納用文字列を宣言
+  Wire.requestFrom(ESP32C3_ADDR,128);               // ESP32C3への送信リクエスト
+  uint8_t received_size = Wire.read();              // 文字列の長さを示すために1文字目に格納しておいたデータを読み出す
+  for(int i=0;i<received_size;i++){                 // 上で読み出した長さの回数分繰り返す
+    char c = Wire.read();                           // 受信バッファから文字として1つ読み出す
+    receive_str += c;                               // 文字列の末尾に格納する
   }
-  int receive_ms = millis() - previous_receive_ms;
+  int receive_ms = millis() - previous_receive_ms;  // 開始時刻との差分を取ってリクエストの処理にかかった時間を計測
 
   // 送信・受信したデータを出力
   Serial.printf("Send : \"%s\"(%dms),  Receive : \"%s\"(%dms) \n", send_str.c_str(), send_ms, receive_str.c_str(), receive_ms);
