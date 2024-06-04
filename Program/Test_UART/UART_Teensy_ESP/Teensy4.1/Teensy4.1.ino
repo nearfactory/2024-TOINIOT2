b914d5d95d5ae33947957c282348649d757bcbf4
nearfactory <yuji070902@gmail.com>
2024-06-04 17:53:45 +0900

    3Dプリンタ出力データを追加した/プログラムデータが更新されちゃうかもしれないから注意して

--- Program/Test_UART/UART_Teensy_ESP/Teensy4.1/Teensy4.1.ino
+++ Program/Test_UART/UART_Teensy_ESP/Teensy4.1/Teensy4.1.ino
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /**********************************************************************
 【ライセンスについて】
 Copyright(c) 2022 by tamanegi
@@ -55,4 +56,86 @@ void loop()
   {
       Serial1.write(Serial.read());
   }
+=======
+/**********************************************************************
+【ライセンスについて】
+Copyright(c) 2022 by tamanegi
+Released under the MIT license
+'http://tamanegi.digick.jp/about-licence/
+
+【マイコン基板】
+ Teensy 4.1
+ 
+【スケッチの説明】
+ COMから読み取った情報を UART8へ出力します。
+ UART8から読み取った情報を COMへ出力します。
+
+【ライブラリ】
+Teensy > Teensy 4.0
+
+【準備】
+ UARTの通信にFT232RLを使用します。
+
+Teensy4.1(COM側) <-> FT232RL(UART2側)
+ GPIO35(UART8 TX) <-> RX
+ GPIO34(UART8 RX) <-> TX
+
+ (Uart番号とオブジェクト名)
+ COM - Serial
+ UART1 - Serial1
+ UART2 - Serial2
+ UART3 - Serial3
+ UART4 - Serial4
+ UART5 - Serial5
+ UART6 - Serial6
+ UART7 - Serial7
+ UART8 - Serial1
+ 
+【バージョン情報】
+2022/12/28 : 新規
+**********************************************************************/
+
+#include<SoftwareSerial.h>
+
+//SoftwareSerial mySerial(0,1);
+
+void setup()
+{
+  // pinMode(0,INPUT_PULLUP);
+  Serial.begin(9600);
+  Serial.println("Teensy4.1");
+  Serial1.begin(9600);
+  // Serial1.setRX(0);
+  // Serial1.setTX(1);
+  mySerial.begin(9600);
+  // Serial1.begin(9600);
+  // pinMode(1,OUTPUT);
+}
+
+void loop()
+{
+  // static int count = 0;
+  // static int ms = 0;
+  // if(millis()-ms >= 500){
+  //   if(count){
+  //     count = 0;
+  //   }else{
+  //     count = 1;
+  //   }
+  //   digitalWrite(1, count);
+  //   ms = millis();
+  // }
+
+  if(Serial1.available() != 0)          //UART5にデータがあれば、読み取った内容をUART0に送信
+  {
+      Serial.write(Serial1.read());
+  }
+
+  if(Serial.available() != 0)           //UART0にデータがあれば、読み取った内容をUART5に送信
+  {
+      Serial1.write(Serial.read());
+  }
+  // Serial.println(count);
+  // delay(50);
+>>>>>>> Stashed changes
 }
