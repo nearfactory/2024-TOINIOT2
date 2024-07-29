8b7680141d6d9d6b36f39d5d7d0e8b9a4ed0887f
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-07-29 20:53:56 +0900

    コンパレータのテストプログラムを追加した

--- Program/Test_Comparetar/Test_Comparetar.ino
+++ Program/Test_Comparetar/Test_Comparetar.ino
@@ -1,3 +1,6 @@
+// IN- > IN+ → 0
+// IN- < IN+ → 1
+
 void setup() {
   pinMode(A0, INPUT_PULLUP);
   Serial.begin(9600);
@@ -6,11 +9,11 @@ void setup() {
 
 void loop() {
   static int hensuu = 128;
-  if(digitalRead(A1)) hensuu += hensuu<255 ? 1 : 0;
-  if(digitalRead(A2)) hensuu -= hensuu>0 ? 1 : 0;
+  if (digitalRead(A1)) hensuu += hensuu < 255 ? 1 : 0;
+  if (digitalRead(A2)) hensuu -= hensuu > 0 ? 1 : 0;
 
   Serial.print("threshold:");
-  Serial.println(hensuu*4);
+  Serial.println(hensuu * 4);
   analogWrite(4, hensuu);
   Serial.println(digitalRead(A0));
   Serial.println(analogRead(A3));
