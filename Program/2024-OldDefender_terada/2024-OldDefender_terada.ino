87cc14120cb82af21151ac78aaf14e34e3d94483
Yuji-Terada <yuji070902@gmail.com>
2024-08-07 10:52:34 +0900

    旧機体の守りプログラムを作成

---
+++ Program/2024-OldDefender_terada/2024-OldDefender_terada.ino
@@ -0,0 +1,21 @@
+#include "pin.h"
+#include "motor.h"
+#include "sensor.h"
+#include "tone.h"
+
+#define DIR_TOLERANCE 8
+#define P_GAIN_DIR 0.95
+#define D_GAIN_DIR 0.7
+
+float dirPrevious = 0;
+float diffDiff = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Program/2024-OldDefender_terada/tone.h
@@ -0,0 +1,11 @@
+// Include Guard
+#ifndef TONE_H
+#define TONE_H
+
+// Declaration
+extern const float soundPitch[24];
+
+// Function
+void playSound(int);
+
+#endif
