de45eab10f2384cf469a5001e73d7412fabf7c81
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-08-30 17:51:02 +0900

    更新

--- Program/Test_MotorP/Test_MotorP.ino
+++ Program/Test_MotorP/Test_MotorP.ino
@@ -26,8 +26,6 @@ void loop() {
   if(btn2_prev && !digitalRead(37))
     motor_goal -= 50;
 
-  // if(digitalRead(36)) motor_goal += 10;
-
   motor_goal = motor_goal > -255 ? motor_goal : -255;
   motor_goal = motor_goal < 255  ? motor_goal :  255;
 
@@ -41,46 +39,6 @@ void loop() {
   Serial.printf("motor_goal:%lf  p_val:%lf  motor_now:", motor_goal, p_val);
   Serial.println(motor_now);
   delay(20);
-}
-
-void motorRaw(double power){
-  analogWrite(MOTOR_EN, power*2.55);
-  digitalWrite(MOTOR_PH, power<0);
-
-  return;
-}
-
-void motorP(double power){
-  static double motor_now = 0;
-  static double motor_goal = 0;
-
-  double p_step = 400;
-  double motor_buf = 0;
-  motor_goal = power;
-  
-  for(int i=0;i<p_step;i++){
-    motor_buf = motor_now + (motor_goal - motor_now) * (i+1) / p_step;
-    motorRaw(motor_buf);
-    Serial.println(motor_goal-motor_now);
-    delay(1);
-  }
-
-  motor_now = motor_goal;
-  return;
-}
-
-void motorFastP(double power){
-  double p_step = 400;
-  motor_goal = power;
-  static double motor_buf = 0;
-  motor_now += (motor_goal - motor_now) / p_step;
-
-  motorRaw(motor_now);
-  Serial.println(motor_now);
-
-  // motor_now = motor_goal;
-  // delay(10);
-  return;
 }
 
 void myP(double power){
