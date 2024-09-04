51eb0c49a8123a00d10e50a2facb6b64b10ec23d
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-09-04 18:13:47 +0900

    姿勢制御用を追加
    ちゃんとした計算式を作る必要がある

--- Program/Test_NewAtacker/SetDir/Motor.hpp
+++ Program/Test_NewAtacker/SetDir/Motor.hpp
@@ -20,7 +20,7 @@ float motor         [MOTOR_NUM] = {0};  // プログラマ用
 float motor_prev    [MOTOR_NUM] = {0};  // 前ループのプログラマ用の値
 float motor_raw     [MOTOR_NUM] = {0};  // モーターに反映するやつ
 
-float motor_p_step              = 8;    // P制御のステップ数
+float motor_p_step              = 4;    // P制御のステップ数
 float motor_p_count [MOTOR_NUM] = {0};  // P制御のループ数カウント
 float motor_p_val   [MOTOR_NUM] = {0};  // P制御の1ステップに変化する値
 
@@ -34,58 +34,6 @@ inline void motorSetup(){
   }
   
   Serial.println("motor setup");
-  return;
-}
-
-inline void moveDir(double dir, short power, bool max_power){
-  short power_big = 0;
-  short power_big_id = 0;
-  // Serial.print("default ");
-  for(int i=0;i<MOTOR_NUM;i++){
-    double t = (dir-45-i*90)*3.14/180.0;
-    motor_raw[i] = sin(t)*power;
-    // Serial.printf("m%d:%d ", i+1, motor[i]);
-    if(max_power){
-      if(abs(motor_raw[i]>power_big)){
-        power_big = motor_raw[i];
-        power_big_id = i;
-      }
-    }
-  }
-  double p = (double)power / abs(motor_raw[power_big_id]);
-  if(!max_power) p = 1.0;
-  // Serial.println();
-  // Serial.printf("big:%d\n",power_big);
-  // Serial.printf("id:%d\n",power_big_id);
-  // Serial.printf("p:%lf\n",p);
-  
-  // Serial.print("max ");
-  for(int i=0;i<MOTOR_NUM;i++){
-    motor_raw[i] *= p;
-    // Serial.printf("m%d:%d ", i+1, motor[i]);
-  }
-  // Serial.println();
-
-  return;
-}
-
-inline void setDir(double dir, double goal_dir, uint8_t power, int blend){
-  short mpDir = 0;
-  dir -= goal_dir;
-  dir = dir < -180 ? dir + 360 : dir;
-  dir = dir > 180 ? dir - 360 : dir;
-
-  blend = blend<0?0:blend;
-  blend = blend>100?100:blend;
-  double P_GAIN_DIR = 0.95;
-  if(!(-10<dir && dir<10)){
-    mpDir = (dir * P_GAIN_DIR);
-    for(int i=0;i<4;i++){
-      // motor[i] = motor[i]*(100-blend)*0.01 + mpDir*blend*0.01;
-      motor_raw[i] += mpDir;
-    }
-  }
-
   return;
 }
 
