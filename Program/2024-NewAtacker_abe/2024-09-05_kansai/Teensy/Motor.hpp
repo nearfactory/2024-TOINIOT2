ebae88c877da05d758ad45baee9e7c8fd1a7a709
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-09-05 17:59:35 +0900

    ボタンによる停止機能を実装した

---
+++ Program/2024-NewAtacker_abe/2024-09-05_kansai/Teensy/Motor.hpp
@@ -0,0 +1,132 @@
+#pragma once
+
+constexpr uint8_t MOTOR_NUM = 4;
+constexpr uint8_t MOTOR_PWM_TYPE = 2;
+const uint8_t MOTOR_PIN[MOTOR_NUM][MOTOR_PWM_TYPE]{
+  {29, 28},
+  {9, 6},
+  {5, 4},
+  {3, 2}
+};
+enum MOTOR : uint8_t{
+  EN = 0,
+  PH,
+};
+
+
+// -100~100[%] の範囲で指定する
+float motor         [MOTOR_NUM] = {0};  // プログラマ用
+
+float motor_prev    [MOTOR_NUM] = {0};  // 前ループのプログラマ用の値
+float motor_raw     [MOTOR_NUM] = {0};  // モーターに反映するやつ
+
+float motor_p_step              = 8;    // P制御のステップ数
+float motor_p_count [MOTOR_NUM] = {0};  // P制御のループ数カウント
+float motor_p_val   [MOTOR_NUM] = {0};  // P制御の1ステップに変化する値
+
+float move_dir                  = 0;
+
+inline void motorSetup(){
+  for(int i=0;i<MOTOR_NUM;i++){
+    pinMode(MOTOR_PIN[i][MOTOR::EN], OUTPUT);
+    pinMode(MOTOR_PIN[i][MOTOR::PH], OUTPUT);
+    analogWriteFrequency(MOTOR_PIN[i][MOTOR::EN], 100000);
+  }
+  
+  Serial.println("motor setup");
+  return;
+}
+
+inline void motorRaw(float m1, float m2, float m3, float m4){
+  motor[0] = m1;
+  motor[1] = m2;
+  motor[2] = m3;
+  motor[3] = m4;
+
+  return;
+}
+
+inline void moveDir(double dir, short power, bool max_power){
+  short power_big = 0;
+  short power_big_id = 0;
+  // Serial.print("default ");
+  for(int i=0;i<MOTOR_NUM;i++){
+    double t = (dir-45-i*90)*3.14/180.0;
+    motor_raw[i] = sin(t)*power;
+    // Serial.printf("m%d:%d ", i+1, motor[i]);
+    if(max_power){
+      if(abs(motor_raw[i]>power_big)){
+        power_big = motor_raw[i];
+        power_big_id = i;
+      }
+    }
+  }
+  double p = (double)power / abs(motor_raw[power_big_id]);
+  if(!max_power) p = 1.0;
+  // Serial.println();
+  // Serial.printf("big:%d\n",power_big);
+  // Serial.printf("id:%d\n",power_big_id);
+  // Serial.printf("p:%lf\n",p);
+  
+  // Serial.print("max ");
+  for(int i=0;i<MOTOR_NUM;i++){
+    motor_raw[i] *= p;
+    // Serial.printf("m%d:%d ", i+1, motor[i]);
+  }
+  // Serial.println();
+
+  return;
+}
+
+inline void setDir(double dir, double goal_dir, uint8_t power, int blend){
+  short mpDir = 0;
+  dir -= goal_dir;
+  dir = dir < -180 ? dir + 360 : dir;
+  dir = dir > 180 ? dir - 360 : dir;
+
+  blend = blend<0?0:blend;
+  blend = blend>100?100:blend;
+  double P_GAIN_DIR = 0.95;
+  if(!(-10<dir && dir<10)){
+    mpDir = (dir * P_GAIN_DIR);
+    for(int i=0;i<4;i++){
+      // motor[i] = motor[i]*(100-blend)*0.01 + mpDir*blend*0.01;
+      motor_raw[i] += mpDir;
+    }
+  }
+
+  return;
+}
+
+inline void motorP(){
+  for(int i = 0; i < MOTOR_NUM; i++){
+    if(motor_prev[i] == motor[i]  &&  motor_p_count[i] < motor_p_step){
+      motor_raw[i] += motor_p_val[i];
+      motor_p_count[i]++;
+      // Serial.print("moving");
+    }else if(motor_prev[i] != motor[i]){
+      motor_p_val[i] = (motor[i] - motor_raw[i]) / motor_p_step;
+
+      motor_raw[i] += motor_p_val[i];
+      motor_p_count[i] = 1;
+      // Serial.print("differ");
+    }else{
+      // Serial.print("none");
+    }
+
+    motor_prev[i] = motor[i];
+  }
+  return;
+}
+
+inline void motorRaw(){
+  for(int i=0;i<MOTOR_NUM;i++){
+      motor_raw[i] = motor_raw[i]<-100.0 ? -100.0 : motor_raw[i];
+      motor_raw[i] = 100.0<motor_raw[i]  ?  100.0 : motor_raw[i];
+
+      digitalWrite(MOTOR_PIN[i][MOTOR::PH], motor_raw[i]>0);
+      analogWrite(MOTOR_PIN[i][MOTOR::EN], (uint8_t)abs(motor_raw[i]*255.0/100.0));
+  }
+
+  return;
+}
---
+++ Program/2024-NewAtacker_abe/2024-09-05_kansai/Teensy/Teensy.ino
@@ -0,0 +1,139 @@
+// new atacker main program
+// Teensy4.1
+// 2024-07-30
+
+#include <Wire.h>
+
+#include "Ball.hpp"
+#include "Communication.hpp"
+#include "Dir.hpp"
+#include "Kicker.hpp"
+#include "Motor.hpp"
+#include "UI.hpp"
+
+using namespace std;
+
+void setup() {
+  // 通信/ボールセンサ/方向センサ/キッカー/モーター/UIのセットアップ
+  communicationSetup();
+  ballSetup();
+  dirSetup();
+  kickerSetup();
+  motorSetup();
+  UISetup();
+
+  // BNO055のキャリブレーション
+  /*
+  ・必ず試合開始までに済ませる
+  ・ロボットを回すときに落とさないように注意
+  ・一通り回した後は床などの動かない場所（コート上は×）にて安定させる
+  ・キャリブレーション完了後に再度少し水平に回転・安定させる
+  ・ディスプレイに表示される角度が放置しても変わらなくなったらキャリブレーション完了
+  */
+
+  // ディスプレイモードを方向センサ用に変更
+  DISPLAY_MODE = DISPLAY_MODE::DIR;
+  // キャリブレーションの状況変数を初期化 
+  uint8_t system = 0, gyro = 0, accel = 0, mag = 0;
+  // キャリブレーション完了まで繰り返し
+  while(system!=3 || gyro!=3 || mag!=3){
+    display.clearDisplay();
+    printd(8,8,"calibration...");
+    Serial.println("calibration...");
+
+    // キャリブレーション状況を取得・ディスプレイバッファへ書き込み
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+    printd(32,24,"system:"+to_string(system));
+    printd(32,32,"gyro  :"+to_string(gyro));
+    printd(32,40,"accel :"+to_string(accel));
+    printd(32,48,"mag   :"+to_string(mag));
+
+    // ディスプレイを更新
+    display.display();
+    delay(10);
+  }
+
+  // スタート画面を表示
+  display.clearDisplay();
+  printd(120,56,"start",TEXT_ALIGN::RIGHT, TEXT_ALIGN::BOTTOM);
+  display.display();
+
+  // 3番ボタンが押された → ディスプレイなし
+  // 4番ボタンが押された → ディスプレイあり
+  // HIGH→LOWになったらループ脱出
+  while(!buttonUp(3) && !buttonUp(4)){
+    buttonUpdate();
+    if(button[2]) use_display = false;
+    if(button[3]) use_display = true;
+  }
+
+  display.clearDisplay();
+  display.display();
+
+  
+  // 攻め方向を取得・更新
+  dirUpdate();
+  default_dir = dir;
+  dir_default_display = -dir;
+  
+}
+
+void loop() {
+
+  static auto begin_ms = millis();
+  if(use_display){
+    digitalWrite(LED_BUILTIN, HIGH);
+    display.clearDisplay();
+    clearVariables();
+  }
+
+  // ディスプレイ不使用時
+  if(!use_display){
+    if(buttonUp(3)){
+      previous_button[2] = 0;
+      // 再度ボタンを押すと再開
+      while(!buttonUp(3)){
+        motorRaw(0.0f,0.0f,0.0f,0.0f);
+        motorP();
+        motorRaw();
+        buttonUpdate();
+        delay(40);
+      }
+    }
+  }
+  // データを更新
+  ballUpdate(BALL::DIR);
+  buttonUpdate();
+  dirUpdate();
+  // subUpdate();
+  
+  // 回り込み
+
+
+  // ボールを保持している
+  if(ball_holding) {
+  }
+
+  // ボールが存在しない
+  if(!ball_exist) motorRaw(0.0f,0.0f,0.0f,0.0f);
+  
+  // 姿勢制御
+  // setDir(dir,default_dir,60,40);
+  
+  // モーターに適用
+  motorRaw(16.0f,16.0f,16.0f,16.0f);
+  motorP();
+  motorRaw();
+
+
+  // UI (display)
+  if(use_display){
+    addVariables("process", millis()-begin_ms);
+    addVariables("ball_hold",millis()-ball_hold_begin);
+    begin_ms = millis();
+    if(buttonUp(4)) DISPLAY_MODE = (DISPLAY_MODE+1)%DISPLAY_MODE_NUM;
+    debugDisplay(DISPLAY_MODE);
+    display.display();
+  }
+  
+}
