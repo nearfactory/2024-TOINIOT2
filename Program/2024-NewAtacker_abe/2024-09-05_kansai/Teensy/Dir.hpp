945558d94a5d546c3d3fbb6e845c4ab9cf77c4b1
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-09-07 17:15:47 +0900

    ラインセンサのテストプログラムを更新した
    多分これで動く

--- Program/2024-NewAtacker_abe/2024-09-05_kansai/Teensy/Dir.hpp
+++ Program/2024-NewAtacker_abe/2024-09-05_kansai/Teensy/Dir.hpp
@@ -8,11 +8,15 @@ Adafruit_BNO055 bno(55, 0x28, &Wire2);
 double default_dir = 0.0;
 double dir = 0.0;           // -180 - 180
 double prev_dir = 0.0;
-double dir_default_display = 0.0;
+
 double accel_x = 0.0;
 double accel_y = 0.0;
 double accel_z = 0.0;
 double accel_sum = 0.0;
+
+double atk_x = 0.0;
+double atk_y = 0.0;
+// 自陣側左下を(0,0), 敵陣側右上を(1,1)
 
 inline void dirSetup(){
   if(!bno.begin()){
@@ -32,20 +36,23 @@ inline void dirCalibration(uint8_t system, uint8_t gyro, uint8_t accel, uint8_t 
 
 inline void dirUpdate(){
   prev_dir = dir;
-  sensors_event_t orientation{};
-  bno.getEvent(&orientation, Adafruit_BNO055::VECTOR_EULER);
-  dir = orientation.orientation.x-default_dir;
+  sensors_event_t dir_data{};
+  bno.getEvent(&dir_data, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&dir_data, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  // bno.getEvent(&dir_data, Adafruit_BNO055::)
+
+  // 角度を更新 [-180~180]
+  dir = dir_data.orientation.x-default_dir;
   if(dir<-180) dir += 360;
   if(dir>180)  dir -= 360;
-  // dir = 
 
-  sensors_event_t accel{};
-  bno.getEvent(&accel, Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  accel_x = accel.acceleration.x;
-  accel_y = accel.acceleration.y;
-  accel_z = accel.acceleration.z;
-
+  // 加速度を更新
+  accel_x = dir_data.acceleration.x;
+  accel_y = dir_data.acceleration.y;
+  accel_z = dir_data.acceleration.z;
   accel_sum = accel_x+accel_y+accel_z;
+
+  // 位置を計算
 
   return;
 }
--- Program/Test_NewAtacker/Line/AtMega/AtMega.ino
+++ Program/Test_NewAtacker/Line/AtMega/AtMega.ino
@@ -4,6 +4,18 @@ const     uint8_t LINE_PIN[LINE_NUM] = {
 };
 bool line[LINE_NUM]{false};
 
+constexpr uint8_t ANALOG_NUM = 8;
+const     uint8_t ANALOG_PIN[ANALOG_NUM] = {
+  97, 96, 95, 94, 93, 92, 91, 90
+};
+
+constexpr uint8_t LED_NUM = 3;
+const     uint8_t LED_PIN[LED_NUM] = { 35,36,37 };
+          bool    LED[LED_NUM] = {false};
+
+constexpr uint8_t THRESHOLD_PIN = 5;
+          short   THRESHOLD = 0;
+
 char receive_str[10];
 
 void setup() {
@@ -13,6 +25,8 @@ void setup() {
   Serial1.begin(115200);
   Serial1.println("AtMega");
 
+  // 通信テスト
+  // ********* countの処理をなんとかする
   while(true){
     int count = 0;
     if(Serial1.available()){
@@ -26,12 +40,32 @@ void setup() {
       if(receive_str[i] != teensy[i]) break;
 
       correct = true;
+      digitalWrite(LED_PIN[0], HIGH);
     }
 
     if(correct) break;
   }
 
   for(auto p:LINE_PIN) pinMode(p, INPUT);
+  
+  // 8/17 18:30~
+  // ラインセンサ しきい値
+  auto ms = millis();
+  short max = 0;
+  short mix = 1023;
+  while(millis()-ms<5000){
+    for(auto p:LINE_PIN){
+      short val = analogRead(p);
+
+      min = min>p ? p : min;
+      max = max<p ? p : max;
+    } 
+    delay(100);
+  }
+  THRESHOLD = (min+max)/2;
+  analogWrite(THRESHOLD_PIN, THRESHOLD);
+
+  digitalWrite(LED_PIN[1], HIGH);
 }
 
 void loop() {
