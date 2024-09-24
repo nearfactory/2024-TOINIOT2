0c9dd7838938873c24b93b4d7bb1112e05877c28
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-09-24 18:39:46 +0900

    update: ESP32 DevKitCのUART通信用ファイルを追加した
    HardwareSerialならできる

--- Program/Test_NewAttacker/UART/Teensy/Teensy.ino
+++ Program/Test_NewAttacker/UART/Teensy/Teensy.ino
@@ -1,20 +1,41 @@
+// #include<Wire.h>
+
+// #include<Adafruit_SSD1306.h>
+// #include<Adafruit_GFX.h>
+
+// Adafruit_SSD1306 display(128,64,&Wire2,-1);
+
 void setup(){
   // uart
-  Serial2.begin(9600);
+  Serial2.begin(115200);
 
   // test
   // pinMode(0,INPUT);
   // pinMode(1,OUTPUT);
 
   Serial.begin(9600);
+
+  // Wire2.begin();
+  // while(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c)){
+  //   ;
+  // }
+
+  // display.clearDisplay();
+  // display.setTextSize(1);
+  // display.setTextColor(SSD1306_WHITE);
+  // display.display();
 }
 
 void loop(){
+  // display.clearDisplay();
+  // display.setCursor(0,8);
+  // display.println("uart test");
   // uart
   while(Serial2.available()){
     char c = Serial2.read();
     //Serial.println(Serial1.read());
     Serial.print(c);
+    // display.print(c);
   }
   /*
   if(Serial.available()){
@@ -33,5 +54,6 @@ void loop(){
   // Serial.println(digitalRead(0));
   // digitalWrite(1,val);
   
+  // display.display();
   delay(50);
 }
