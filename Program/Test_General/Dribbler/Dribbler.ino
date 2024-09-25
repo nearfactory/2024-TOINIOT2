1811532c7565a716366a2bec7b480b2c3413f23e
NearFactory@SciencePC2 <yuji070902@gmail.com>
2024-09-25 18:04:35 +0900

    update:ボールの角度によるズレを修正するやつ

---
+++ Program/Test_General/Dribbler/Dribbler.ino
@@ -0,0 +1,15 @@
+#define EN 32
+#define PH 27
+
+void setup() {
+  pinMode(EN, OUTPUT);
+  pinMode(PH, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(PH,0);
+  analogWrite(EN,128);
+  
+  delay(50);
+}
