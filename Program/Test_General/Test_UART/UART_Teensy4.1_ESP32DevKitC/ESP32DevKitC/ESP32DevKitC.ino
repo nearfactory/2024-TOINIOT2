488c93ed9d9c7e713639a50fa7add7ce302e4b37
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-09-27 22:51:14 +0900

    update: UARTのテストプログラムを更新

--- Program/Test_General/Test_UART/UART_Teensy4.1_ESP32DevKitC/ESP32DevKitC/ESP32DevKitC.ino
+++ Program/Test_General/Test_UART/UART_Teensy4.1_ESP32DevKitC/ESP32DevKitC/ESP32DevKitC.ino
@@ -1,8 +1,14 @@
-void setup() {
+HardwareSerial mySerial1();
+HardwareSerial mySerial2();
+HardwareSerial mySerial3();
 
+void setup() {
+  mySerial1.begin(9600,SERIAL_8N1,32,33);
+  mySerial2.begin(9600,SERIAL_8N1,32,33);
+  mySerial3.begin(9600,SERIAL_8N1,32,33);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
