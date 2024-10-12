a509cfcf8047cb62a0c77c49e00342c8a2be38e5
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-10-12 19:38:04 +0900

    add: 白線のベクトルを算出するプログラムを書いた
    しおからさんブログ参考

--- Program/Test_NewAttacker/Line/ATmega/ATmega.ino
+++ Program/Test_NewAttacker/Line/ATmega/ATmega.ino
@@ -1,88 +1,43 @@
-constexpr uint8_t LINE_NUM = 32;
+constexpr uint8_t LINE_NUM = 46;
+constexpr uint8_t INNER_NUM = 26;
 const     uint8_t LINE_PIN[LINE_NUM] = {
-  12,13,14,15,16,17,18,27, 19,20,21,22,23,24,25,26, 53,54,55,56,57,58,59,60, 63,64,65,66,67,68,69,79
-};
-bool line[LINE_NUM]{false};
-
-constexpr uint8_t ANALOG_NUM = 8;
-const     uint8_t ANALOG_PIN[ANALOG_NUM] = {
-  97, 96, 95, 94, 93, 92, 91, 90
+  // 
 };
 
-constexpr uint8_t LED_NUM = 3;
-const     uint8_t LED_PIN[LED_NUM] = { 35,36,37 };
-          bool    LED[LED_NUM] = {false};
+bool line[LINE_NUM];
 
-constexpr uint8_t THRESHOLD_PIN = 5;
-          short   THRESHOLD = 0;
-
-char receive_str[10];
-
-void setup() {
+void setup(){
   Serial.begin(9600);
-  Serial.println("TOINIOT2 Line Test Atmega 2024-09-04");
-
-  Serial1.begin(115200);
-  Serial1.println("AtMega");
-
-  // 通信テスト
-  // ********* countの処理をなんとかする
-  /*
-  while(true){
-    int count = 0;
-    if(Serial1.available()){
-      receive_str[count] = Serial.read();
-      count++;
-    }
-    
-    bool correct = false;
-    char teensy[] = "Teensy";
-    for(int i=0;i<7;i++){
-      if(receive_str[i] != teensy[i]) break;
-
-      correct = true;
-      digitalWrite(LED_PIN[0], HIGH);
-    }
-
-    if(correct) break;
-  }
-  */
+  Serial.println("TOINIOT2 Line 2024-10-12");
 
   for(auto p:LINE_PIN) pinMode(p, INPUT);
-  
-  // 8/17 18:30~
-  // ラインセンサ しきい値
-  auto ms = millis();
-  short max = 0;
-  short mix = 1023;
-  while(millis()-ms<5000){
-    for(auto p:LINE_PIN){
-      short val = analogRead(p);
-
-      min = min>p ? p : min;
-      max = max<p ? p : max;
-    } 
-    delay(100);
-  }
-  THRESHOLD = (min+max)/2;
-  analogWrite(THRESHOLD_PIN, THRESHOLD);
-
-  digitalWrite(LED_PIN[1], HIGH);
 }
 
-void loop() {
-  while(!Serial1.available());
+void loop(){
+  // 白線を取得
+  for(int i=0;i<LINE_NUM;i++) line[i] = digitalRead(LINE_PIN[i]);
 
-  while(Serial1.available()){
-    if(Serial1.read()=='T') break;
+  // 白線のベクトルを計算
+  // しおからさんのアルゴリズムは複雑で実装がめんどくさい
+  // → 反応したセンサのベクトルの平均値を白線のベクトルとする代替アルゴリズムを検証
+  double x = 0.0;
+  double y = 0.0;
+  uint8_t responded = 0;
+  for(int i=0;i<INNER_NUM;i++){
+    if(line[i]){
+      double sensor_dir = radians(i*360.0/INNER_NUM - 90.0); // rad
+      x += cos(sensor_dir);
+      y += sin(sensor_dir);
+      responded++;
+    } 
   }
+  x /= responded;
+  y /= responded;
 
-  char send_str[7]={
-    0b10101010, 0, 0, 0, 0, 0b01010101, \0
-  };
-  for(int i=0;i<LINE_NUM;i++){
-    send_str[1+i/8] = send_str[1+i/8] | digitalRead(LINE_PIN[i]) << (i%8); 
-  }
-
-  Serial1.println(send_str);
+  Serial.print("x:");
+  Serial.print(x);
+  Serial.print("\ty:");
+  Serial.print(y);
+  Serial.print("\tdir:");
+  Serial.print(degrees(atan2(y,x)));
 }
