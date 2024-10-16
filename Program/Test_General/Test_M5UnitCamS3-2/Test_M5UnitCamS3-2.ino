e6bfd8924469f3cd1e3c18fa92896caf7135d261
NearFactory@SciencePC2 <yuji070902@gmail.com>
2024-10-16 19:05:15 +0900

    update: 物体認識のプログラムを更新
    setup()のメモリの確保で落ちる

--- Program/Test_General/Test_M5UnitCamS3-2/Test_M5UnitCamS3-2.ino
+++ Program/Test_General/Test_M5UnitCamS3-2/Test_M5UnitCamS3-2.ino
@@ -22,6 +22,8 @@ RGBが正しく取得できない原因
 
 */
 
+#include <vector>
+
 #include <Arduino.h>
 #include <esp_camera.h>
 
@@ -41,6 +43,32 @@ RGBが正しく取得できない原因
 SoftwareSerial SoftSerial;
 
 camera_fb_t* Camera_fb;
+
+struct Obj{
+  int id=-1;
+  uint32_t x=-1, y=-1, count=1;
+};
+
+int8_t* buf = NULL;
+
+// 塗りつぶす関数
+void fill(int x, int y, int id){
+  buf[x+y*320]=id; /* ー１を置く */
+
+    if (buf[x+(y-1)*320]==-1) /* 上に自分と同じ色があるか */
+        fill(x,y-1, id);     /* あればその座標で再帰呼び出し */
+
+    if (buf[x+1+y*320]==-1) /* 右 */
+        fill(x+1,y, id);
+
+    if (buf[x+(y+1)*320]==-1) /* 下 */
+        fill(x,y+1, id);
+
+    if (buf[x-1+y*320]==-1) /* 左 */
+        fill(x-1,y, id);
+      
+    return;
+}
 
 void setup() {
   // SoftSerial.begin(BAUDRATE, SWSERIAL_8N1, SOFT_RXD, SOFT_TXD, false, 256);
@@ -113,7 +141,7 @@ void setup() {
 
   // ホワイトバランスを自動に設定
   // ホワイトバランスをオンにするとRGBが正しく取得できないため、自動設定をオフに
-  s->set_whitebal(s, 1);  // 0 = Off, 1 = On
+  s->set_whitebal(s, 0);  // 0 = Off, 1 = On
 
   // 上下方向の反転の有無
   s->set_vflip(s, 0);  // 0 = 正常, 1 = 上下反転
@@ -138,6 +166,16 @@ void setup() {
   //   }
   //   esp_camera_fb_return(Camera_fb);  // フレームバッファを解放
   // }
+  
+
+
+  Serial.println("\n\n\nmem\n\n\n");
+  buf = reinterpret_cast<int8_t*>(malloc(sizeof(int8_t)*320*240)); // しきい値を超えたピクセルのバッファ
+  if(buf == NULL){
+    Serial.println("buf err!");
+    return;
+  } 
+
   delay(100);
 
 }
@@ -152,47 +190,50 @@ void loop() {
     return;
   }
 
+
+  std::vector<Obj> obj(0);
+
+  // しきい値を超えたピクセルのバッファを作る
   uint8_t threshold = 24;
-  uint32_t x=0, y=0, count=1;
   for(int i=0;i<Camera_fb->len/2;i++){
     uint16_t pixel = (Camera_fb->buf[i*2]<<8) + (Camera_fb->buf[i*2+1]);
     uint8_t r = (pixel & 0b1111100000000000) >> 11;
     uint8_t g = (pixel & 0b0000011111100000) >> 6;
     uint8_t b = (pixel & 0b0000000000011111);
 
+    // 仮でオブジェクトIDを-1に設定
     if(b > threshold){
-      x += i%320;
-      y += i/320;
-      count++;
+      buf[i] = -1;
+    }else{
+      buf[i] = 0;
+    }
+  }
+
+  // 物体認識
+  // https://www.etcnotes.info/almath/algofill.html
+  int next_id = 1;
+  for(int y=0;y<240;y++){
+    for(int x=0;x<320;x++){
+      int i = x+320*y;
+      if(buf[x+320*y] == -1){
+        fill(x,y,next_id);
+        next_id++;
+      }
     }
   }
   
-  // Serial.printf("x:%03d y:%03d count:%08d    ", x/count, y/count, count);
-  Serial.printf("x:%lf y:%lf count:%08d    ", x/count/320.0, y/count/240.0, count);
+  Serial.printf("id:%d  ", next_id);
 
   uint16_t main = (Camera_fb->buf[76800]<<8) + (Camera_fb->buf[76801]);
-  // main = 0b1111100000000000;
   Serial.print(" Min:");
   Serial.print(0);
   Serial.print(" Max:");
   Serial.print(31);
-  Serial.printf(" R:%02d", main >> 11);
-  // Serial.print(main >> 11);
+  Serial.printf(" R:%02d", (main >> 11));
   Serial.printf(" G:%02d", (main & 0b0000011111100000) >> 6);
-  // Serial.print(((main & 0b0000011111100000) >> 5) / 2);
   Serial.printf(" B:%02d", main & 0b0000000000011111);
-  // Serial.print(main & 0b0000000000011111);
-
-  // Serial.printf("len:%d\t", Camera_fb->len);
-  // for(int i=0;i<16;i++){
-  //   Serial.print(main>>(15-i)&1);
-  //   if(i==4) Serial.print(" ");;
-  //   if(i==10) Serial.print(" ");
-  // }
   
   esp_camera_fb_return(Camera_fb);
-
-  // delay(250);
 
   Serial.println();
 }
