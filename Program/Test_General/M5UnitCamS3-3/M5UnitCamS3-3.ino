c25d86f383f88f9df9db4d18d6bf0de650e92eff
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-11-06 18:03:48 +0900

    update: カメラのENピンの実験プログラムを更新した

--- Program/Test_General/M5UnitCamS3-3/M5UnitCamS3-3.ino
+++ Program/Test_General/M5UnitCamS3-3/M5UnitCamS3-3.ino
@@ -1,14 +1,15 @@
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 
-#define SOFT_RXD 19
-#define SOFT_TXD 20
-SoftwareSerial SoftSerial;
+// #define SOFT_RXD 19
+// #define SOFT_TXD 20
+// SoftwareSerial Serial;
 
 void setup() {
-  SoftSerial.begin(115200, SWSERIAL_8N1, SOFT_RXD, SOFT_TXD, false, 256);
+  // Serial.begin(115200, SWSERIAL_8N1, SOFT_RXD, SOFT_TXD, false, 256);
+  Serial.begin(115200);
 }
 
 void loop() {
-  SoftSerial.println("M5 Stamp Cam S3");
+  Serial.println("M5 Stamp Cam S3");
   delay(50);
 }
