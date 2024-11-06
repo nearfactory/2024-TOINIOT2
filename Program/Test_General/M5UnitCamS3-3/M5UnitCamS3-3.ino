56edc8cf1150b27c4eec3b564f18b0db752ffe85
NearFactory@SciencePC2 <yuji070902@gmail.com>
2024-11-06 17:56:05 +0900

    add: カメラのシリアルテスト用プログラムを追加

---
+++ Program/Test_General/M5UnitCamS3-3/M5UnitCamS3-3.ino
@@ -0,0 +1,14 @@
+#include <SoftwareSerial.h>
+
+#define SOFT_RXD 19
+#define SOFT_TXD 20
+SoftwareSerial SoftSerial;
+
+void setup() {
+  SoftSerial.begin(115200, SWSERIAL_8N1, SOFT_RXD, SOFT_TXD, false, 256);
+}
+
+void loop() {
+  SoftSerial.println("M5 Stamp Cam S3");
+  delay(50);
+}
