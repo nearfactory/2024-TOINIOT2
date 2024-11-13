eef5ef0c433551977774ffc2d76d24c435c0b029
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-11-13 18:50:35 +0900

    update: TeensyとUnitCamS3のUART通信をしようとした
    前にやった

--- Program/Test_General/M5UnitCamS3-3/M5UnitCamS3-3.ino
+++ Program/Test_General/M5UnitCamS3-3/M5UnitCamS3-3.ino
@@ -1,15 +1,15 @@
-// #include <SoftwareSerial.h>
+#include <SoftwareSerial.h>
 
-// #define SOFT_RXD 19
-// #define SOFT_TXD 20
-// SoftwareSerial Serial;
+#define SOFT_RXD 19
+#define SOFT_TXD 20
+SoftwareSerial mySerial(SOFT_RXD, SOFT_TXD);
 
 void setup() {
-  // Serial.begin(115200, SWSERIAL_8N1, SOFT_RXD, SOFT_TXD, false, 256);
-  Serial.begin(115200);
+  // mySerial.begin(9600, SERIAL_8N1, SOFT_RXD, SOFT_TXD, false, 256);
+  mySerial.begin(9600);
 }
 
 void loop() {
-  Serial.println("M5 Stamp Cam S3");
+  mySerial.println("M5 Stamp Cam S3");
   delay(50);
 }
