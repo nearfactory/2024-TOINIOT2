fe4aae67266d9db5751133dd7505c84277fd24aa
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-11-16 21:01:32 +0900

    add: 地磁気をライブラリ化

--- Program/Test_NewAttacker/FollowBall/Dir.hpp
+++ Program/Test_NewAttacker/FollowBall/Dir.hpp
@@ -3,49 +3,23 @@
 #include <Adafruit_BNO055.h>
 #include <Wire.h>
 
-Adafruit_BNO055 bno(55, 0x28, &Wire2);
+#include "Vec2.hpp"
 
-double default_dir = 0.0;
-double dir = 0.0;           // -180 - 180
-double prev_dir = 0.0;
+class Dir{
+private:
+  Adafruit_BNO055 bno;
 
-double accel_x = 0.0;
-double accel_y = 0.0;
-double accel_z = 0.0;
-double accel_sum = 0.0;
+  float default_dir;
+  float prev_dir;
 
-double atk_x = 0.0;
-double atk_y = 0.0;
-// 自陣側左下を(0,0), 敵陣側右上を(1,1)
+public:
+  float dir;
+  Vec2  accel;
 
-inline void dirSetup(){
-  if(!bno.begin()){
-    Serial.println("bno error!");
-    while(true);
-  }
+  Dir() : bno(55, 0x28, &Wire2), default_dir(0), prev_dir(0), dir(0), accel(0,0) {};
 
-  Serial.println("dir setup");
-  return;
-}
-
-inline void dirUpdate(){
-  prev_dir = dir;
-  sensors_event_t dir_data{};
-  bno.getEvent(&dir_data, Adafruit_BNO055::VECTOR_EULER);
-
-  // 角度を更新 [-180~180]
-  dir = dir_data.orientation.x-default_dir;
-  if(dir<-180) dir += 360;
-  if(dir>180)  dir -= 360;
-
-  // 加速度を更新
-  bno.getEvent(&dir_data, Adafruit_BNO055::VECTOR_ACCELEROMETER);
-  accel_x = dir_data.acceleration.x;
-  accel_y = dir_data.acceleration.y;
-  accel_z = dir_data.acceleration.z;
-  accel_sum = accel_x+accel_y+accel_z;
-
-  // 位置を計算
-
-  return;
-}
+  void begin();
+  void calibration(uint8_t* system, uint8_t* gyro, uint8_t* accel, uint8_t* mag);
+  void setDefault();
+  void read();
+};
