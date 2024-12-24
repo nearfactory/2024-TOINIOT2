df80108098334c95e0b1b8a6bc57e3b8ae2d2669
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-12-24 18:05:24 +0900

    add: メイン-ラインの通信テストプログラムを更新した

---
+++ Program/Test_NewAttacker/Connection/Main-Line/Line/Line.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+
+  Serial.print("atmega:");
+  while(Serial.available()){
+    Serial.print((char)Serial.read());
+  }
+
+  delay(5);
+}
---
+++ Program/Test_NewAttacker/Connection/Main-Line/Main/Main.ino
@@ -0,0 +1,18 @@
+void setup(){
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+  delay(500);
+  Serial.println("2024-12-24 TOINIOT2 Line Connection Test");
+}
+
+void loop(){
+  Serial1.println("Teensy");
+  
+  while(Serial1.available()){
+    Serial.print((char)Serial1.read());
+  }
+  Serial.println();
+
+  delay(10);
+}
