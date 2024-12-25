dd384ab87ff7ede158abffdd1c6f275c5e29d30c
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2024-12-25 20:03:21 +0900

    add: 守り機体用のライントレースのテストプログラムを追加した

---
+++ Program/Test_NewDefender/LineTrace/Ball.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include "Sub.hpp"
+
+class Ball{
+private:
+  static constexpr uint8_t NUM = 16;
+  const uint8_t PIN[NUM] = {
+    // 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,  26, 27, 38, 39, 40, 41
+    A0, A1, A2, A3, A4, A5,  A6, A7, A8, A9,  A12, A13,  A14, A15, A16, A17
+  };
+
+  static constexpr uint16_t DISTANCE_MIN = 7000;
+  static constexpr uint16_t DISTANCE_MAX = 16200;
+  uint16_t distance_h = 13200;
+
+  uint16_t ball[NUM]{0};
+  uint32_t hold_begin = 0;
+  uint32_t not_hold_begin = 0;
+
+public:
+  float    dir = 0;
+  float    dir_prev = 0;
+  uint16_t distance = 0;
+
+  bool     is_exist = true;
+
+  bool     is_hold = false;
+  bool     is_prev_hold = false;
+  uint32_t hold_time = 0;
+  uint32_t not_hold_time = 0;
+
+  Ball() : dir(0), distance(0), is_exist(true), is_hold(false), hold_time(0) {}
+
+  void begin();
+  void read();
+};
+
+extern Ball ball;
---
+++ Program/Test_NewDefender/LineTrace/LineTrace.ino
@@ -0,0 +1,135 @@
+// new atacker kansai block program
+// Teensy4.1
+// 2024-12-24
+
+#include <Wire.h>
+
+#include "Ball.hpp"
+#include "Camera.hpp"
+#include "Dir.hpp"
+#include "Display.hpp"
+#include "Kicker.hpp"
+#include "Line.hpp"
+#include "Motor.hpp"
+#include "Sub.hpp"
+#include "UI.hpp"
+
+Ball ball;
+Camera camera;
+Dir dir;
+Display display;
+Kicker kicker;
+Line line;
+Motor motor;
+Sub sub;
+UI ui;
+
+using namespace std;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("2024-12-24 TOINIOT2 kansai def");
+
+  ball.begin();
+  camera.begin();
+  display.begin();
+  dir.begin();
+  kicker.begin();
+  line.begin(115200);
+  motor.begin();
+  sub.begin();
+  ui.begin();
+  
+  // calibration
+  uint8_t system=0, gyro=0, accel=0, mag=0;
+  while(system<3 || gyro<3 || mag<3){
+    dir.calibration(&system, &gyro, &accel, &mag);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    display.printd(32,16,"system: "+to_string(system));
+    display.printd(32,24,"gyro  : "+to_string(gyro));
+    display.printd(32,32,"accel : "+to_string(accel));
+    display.printd(32,40,"mag   : "+to_string(mag));
+
+    display.printd(120, 8, "skip", ALIGN::RIGHT);
+    ui.read();
+    if(ui.buttonUp(1)) break;
+
+    display.draw();
+  }
+
+  // 攻め方向が設定されるまで待機
+  display.printd(8,56,"set dir");
+  display.draw();
+  while(!ui.buttonUp(0)){ ui.read(); }
+  
+  dir.setDefault();
+}
+
+
+bool is_display_on = true;
+
+
+void loop() {
+  // delayなしで3(ms)
+
+  ball.read();
+  camera.read();
+  dir.read();
+  line.read();
+  sub.read();
+  ui.read();
+
+
+  if(digitalRead(ui.TOGGLE_PIN)){
+    if(ui.buttonUp(0)) display.next();
+
+    // display.addValiables("p_gain :"+to_string(p_gain), &p_gain);
+    // display.addValiables("d_gain :"+to_string(d_gain), &d_gain);
+
+    display.debug();
+    display.draw();
+
+    motor.set(0,0,0,0);
+    is_display_on = true;
+
+  }else{
+    // ディスプレイを消灯
+    if(is_display_on){
+      display.draw();
+      is_display_on = false;
+    }
+
+
+    float dir = 0;
+    if(ball.dir > 0)  dir = 90.0;
+    else              dir = -90.0;
+
+    int power = (int) abs(sin(radians(ball.dir))) * 150.0;
+
+    moveDir(dir, power);
+
+
+
+    // 姿勢制御
+
+    float p_gain = 0.64f;
+    float d_gain = 0.45f;
+    float dir_power = dir_power = (dir.dir) * p_gain - (dir.prev_dir - dir.dir) * d_gain;
+
+    if(abs(dir.dir) > 45) {
+      // 故障復帰
+      motor.set(dir_power, dir_power, dir_power, dir_power);
+    }else{
+      // 姿勢制御
+      motor.add(dir_power, dir_power, dir_power, dir_power);
+    }
+
+  }
+
+
+  motor.avr();
+  motor.write();
+  
+  kicker.write();
+}
