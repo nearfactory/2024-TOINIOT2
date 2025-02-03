73352b27e1b0c595681d1915740ffe760c328060
NearFactory <yuji070902@gmail.com>
2025-02-03 18:21:40 +0900

    update: キッカーのテスト用プログラムを更新した

--- Program/Test_General/Kicker/Kicker.ino
+++ Program/Test_General/Kicker/Kicker.ino
@@ -1,19 +1,32 @@
+constexpr int KICKER_SPAN_MS = 2500;  // 射出タイミング間の待ち時間(ms)
+constexpr int KICKER_THRESHOLD = 400; // キッカー用照度センサのしきい値
+
+long kickerStart = 0; // 射出タイミング間計測用
+
 void setup() {
   pinMode(D3, OUTPUT);
 
   Serial.begin(115200);
-  Serial.println("Kicker Test Program 2025-01-27");
+  Serial.println("Kicker Test Program 2025-02-03");
 }
 
 void loop() {
-  if(analogRead(A1) < 200){
+  Serial.println(analogRead(A1));
+
+  if(analogRead(A1) < KICKER_THRESHOLD){
+    // ボールを保持しているなら
     Serial.println("Holding");
-    digitalWrite(D3, 1);
-    delay(50);
-    digitalWrite(D3, 0);
+    if(millis() - kickerStart > KICKER_SPAN_MS){
+      // 前回の射出から2500ms以上経過しているなら
+      digitalWrite(D3, 1);
+      kickerStart = millis();
+      delay(50);
+      digitalWrite(D3, 0);
+    }
   }
   else{
+    // ボールを保持していないなら
     Serial.println("Waiting");
   }
-  delay(1000);
+  delay(50);
 }
