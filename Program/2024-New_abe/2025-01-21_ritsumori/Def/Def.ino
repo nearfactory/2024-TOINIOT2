b63e38c6963b83718dcb85347bd27219a003b5d1
maguronoosushi0807 <maguronoosushi0807@gmail.com>
2025-02-21 22:31:52 +0900

    update: ボールの前で停まるようにした

--- Program/2024-New_abe/2025-01-21_ritsumori/Def/Def.ino
+++ Program/2024-New_abe/2025-01-21_ritsumori/Def/Def.ino
@@ -36,6 +36,10 @@ State state_prev = State::LineTrace;
 uint32_t state_begin = 0;
 
 
+float p_gain = 1.5;
+float d_gain = 3.0;  // Test
+
+
 void setup() {
   Serial.begin(115200);
   Serial.println("2024-12-24 TOINIOT2 kansai def");
@@ -97,43 +101,27 @@ void loop() {
   sub.read();
   ui.read();
 
-  // 白線に垂直に動くためのベクトル
-  follow_dir = 0;
-  if(line_dir > 0){
-    if(line_dir-180 < ball.dir && ball.dir < line_dir){
-      follow_dir = line_dir - 90.0;
-    }else{
-      follow_dir = line_dir + 90.0;
-    }
-  }else{
-    if(line_dir < ball.dir && ball.dir < line_dir + 180){
-      follow_dir = line_dir + 90.0;
-    }else{
-      follow_dir = line_dir - 90.0;
-    }
-  }
+
 
   if(ui.is_toggle){
     if(ui.buttonUp(0)) display.next();
-    // for(auto b:ball.ball) Serial.printf("%d\t", b);
-    // Serial.println();
 
     // variables
-    // display.addValiables("gain: "+to_string(gain), &gain);
-    display.addValiables("p_gain: "+to_string(dir.p_gain), &dir.p_gain);
-    display.addValiables("d_gain: "+to_string(dir.d_gain), &dir.d_gain);
+    // display.addValiables("p_gain :"+to_string(p_gain), &p_gain);
+    // display.addValiables("d_gain :"+to_string(d_gain), &d_gain);
+    // display.addValiables("diff :"+to_string(difference), &difference);
 
     display.debug();
     display.draw();
 
     state = State::BackToGoal_Strong;
+    // state = State::KeeperDash;
 
     motor.set(0,0,0,0);
     is_display_on = true;
 
     motor.avr();
     motor.write();
-    // delay(15);
 
     return;
   }
@@ -154,20 +142,6 @@ void loop() {
 
 
 
-  // 0.キックオフ
-  /*
-  if(state == State::KickOff){
-    if(abs(dir.dir) > 90){
-      motor.setDir(dir.dir, dir.dir_prev, dir.p_gain, dir.d_gain);
-    }else{
-      // state = State::BackToGoal_Weak;
-      state = State::LineTrace;
-    }
-  }
-  */
-
-
-
   // 1.ライントレース
   if(state == State::LineTrace){
     // ボール追従の角度を算出
@@ -181,18 +155,37 @@ void loop() {
     }
 
 
-    static bool is_block = false, is_side = false;
-    static bool is_stop = false;
+    // ボールと白線の角度の差
+    float b = ball.dir;
+    if(b < -90) b += 180;
+    if(90 < b)  b -= 180;
+
+    float l = line.dir;
+    if(l < -90) l += 180;
+    if(90 < l)  l -= 180;
+
+    float difference = abs(b-l);
+
 
     // ラインと同じ角度にボールがある場合に動かない
+    if(difference < 15.0) {
+      ui.buzzer(880.0f);
+    }else{
+      ui.buzzer(440.0f);
+    }
+
+
+    static bool is_block = false, is_side = false;
+    static bool is_stop = false;
+    
     if(abs(ball.dir - line.dir) < 10 /*|| abs(ball.dir - line.dir) > 170*/) {
-      is_block = true;
+      // is_block = true;
     }else{
       is_block = false;
     }
 
     // ラインが±90°付近のときに動かない
-    if(is_block || is_side){
+    if(is_block || is_side && ball.is_exist){
       // ロボットが敵陣向かって左
       if(0 < camera.def.dir){
         if(-45 < ball.dir)  is_side = false;
@@ -225,35 +218,46 @@ void loop() {
 
       // ベクトルから角度に変換
       float move_dir = degrees(atan2(move.y, move.x));
-      motor.moveDir(move_dir, 90);
 
+      // 角度の差分から出力の強さに変換
+      int min = 10, max = 30, max_power = 90;
+      int power = max_power * (difference - min) / (max - min);
+      if(power < 0) power = 0;
+      if(max_power < power) power = max_power;
+
+      motor.moveDir(move_dir, (uint8_t)power);
       motor.setDirAdd(dir.dir, dir.dir_prev, dir.p_gain, dir.d_gain);
     }
 
 
+    // →  2.キーパーダッシュ
+    if(ball.is_hold) state = State::KeeperDash;
 
+    // 中立点のボールを押しだす
+    // -30~30かつ、<14400 の範囲にボールがある状態が2秒続けばキーパーダッシュに移行
+    static bool is_neutral = false;
+    static uint32_t neutral_begin = millis();
 
-    // // トリガー
-    // static bool     begin = false;
-    // static uint32_t timer = 0;
-    // bool is_dash = false;
+    if(is_neutral == false){
+      // キーパーダッシュに移行する条件
+      if(abs(ball.dir) < 30 && ball.distance < 12000){
+        is_neutral = true;
+        neutral_begin = millis();
+      }
+    }else{
+      if(millis() - neutral_begin > 2000){
+        state = State::KeeperDash;
+        is_neutral = true;
+      }
 
-    // // -30~30かつ、<14400 の範囲にボールがある状態が2秒続けばキーパーダッシュに移行
-    // if(abs(ball.dir)<30 && ball.distance < 13500){
-    //   if(!begin){
-    //     begin = true;
-    //     timer = millis();
-    //   }else{
-    //     if(millis()-timer > 2000) is_dash = true;
-    //   }
-    // }else{
-    //   begin = false;
-    // }
+      // ボールが動かされた判定
+      if(abs(ball.dir) > 30 || ball.distance > 12000){
+        is_neutral = false;
+      }
+    }
 
-    // // →  2.キーパーダッシュ
-    // if(is_dash){
-    //   state = State::KeeperDash;
-    // }
+    
+
     // →  3.ゴール前に戻る(弱め)
     if(!line.on){
       state = State::BackToGoal_Weak;
@@ -264,32 +268,109 @@ void loop() {
 
 
   /*
+  */
   // 2.キーパーダッシュ
   else if(state == State::KeeperDash){
-    motor.moveDir(0,0);
-    if(state_elapsed > 2000){
-      state = State::LineTrace;
-    }
-    float move_dir = 0;
-    if(ball.is_hold){
-      move_dir = camera.atk.dir;
+
+    
+    if(ball.is_hold == false){
+      
+      static float r = 8200;
+      static float h = 10;
+
+      float move_dir = 0;
+
+
+      if(abs(ball.dir)<h){
+        move_dir = ball.dir * p_gain - d_gain*(ball.dir - ball.dir_prev);
+        h = 20;
+      }
+      // 円周上
+      else if(ball.distance < r){
+        float theta = 90 + (r-ball.distance) * 90 / r;
+        move_dir = ball.dir + (ball.dir>0?theta:-theta);
+        h = 10;
+      }
+      //接線
+      else{
+        float theta = degrees(asin(r / ball.distance));
+        move_dir = ball.dir + (ball.dir>0?theta:-theta);
+        h = 10;
+      }
+      motor.moveDir(move_dir, 60);
+      motor.setDirAdd(dir.dir, dir.dir_prev, dir.p_gain, dir.d_gain);
+
     }else{
-      move_dir = ball.dir * 1.5 - 4.2*(ball.dir - ball.dir_prev);
+      motor.moveDir(camera.chance_dir, 80);
+      motor.setDirAdd(dir.dir, dir.dir_prev, dir.p_gain, dir.d_gain);
+
+      // if(camera.atk.h > 30){
+      // }
+        sub.kick();
+        state = State::BackToGoal_Strong;
     }
-    motor.moveDirFast(move_dir, 100);
-
-    motor.setDirAdd(camera.atk.dir, camera.atk.dir_prev, dir.p_gain, dir.d_gain);
 
 
-    bool is_line = line.on && abs(line.dir) < 90;
-    bool is_stop = false;
+    // 白線処理    
+    if(line.on && state_elapsed > 200){
+
+      float avoid_dir = 0;
+
+      // 後ろ
+      if(line.dir > 157.5 || line.dir < -157.5){
+        if(abs(ball.dir) < 5) avoid_dir = 0;
+        else if(ball.dir < 0) avoid_dir = 45;
+        else                  avoid_dir = -45;
+      }
+      // 左後ろ
+      else if(line.dir < -112.5){
+        if(-135 < ball.dir && ball.dir < 45)  avoid_dir = 0;
+        else                                  avoid_dir = 90;
+      }
+      // 左
+      else if(line.dir < -67.5){
+        if(-90 < ball.dir && ball.dir < 90)   avoid_dir = 45;
+        else                                  avoid_dir = 135;
+      }
+      // 左前
+      else if(line.dir < -22.5){
+        if(-45 < ball.dir && ball.dir < 135)  avoid_dir = 90;
+        else                                  avoid_dir = 180;
+      }
+      // 正面
+      else if(line.dir < 22.5){
+        if(abs(ball.dir) < 5) avoid_dir = 180;
+        else if(ball.dir < 0) avoid_dir = 135;
+        else                  avoid_dir = -135;
+      }
+      // 右前
+      else if(line.dir < 67.5){
+        if(-135 < ball.dir && ball.dir < 45)  avoid_dir = -90;
+        else                                  avoid_dir = -180;
+      }
+      // 右
+      else if(line.dir < 112.5){
+        if(-90 < ball.dir && ball.dir < 90) avoid_dir = -45;
+        else                                avoid_dir = -135;
+      }
+      // 右後ろ
+      else{
+        if(-45 < ball.dir && ball.dir < 135)  avoid_dir = 0;
+        else                                  avoid_dir = -90;
+      }
+
+      motor.moveDir(avoid_dir, 90);
+      motor.setDirAdd(dir.dir, dir.dir_prev, dir.p_gain, dir.d_gain);
+    }
+
+
+    // if(state_elapsed > 3000) state = State::LineTrace;
 
     // → 4.ゴール前に戻る(強め)
-    if(is_line || is_stop){
-      state = State::BackToGoal_Strong;
-    }
+    // if(is_line || is_stop){
+    //   state = State::BackToGoal_Strong;
+    // }
   }
-  */
   
 
 
